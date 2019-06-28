1c94b2f7ae36708642ff50662f8d37132bdd35b2
pengyu <pengy70@chinaunicom.cn>
2019-06-28 10:58:54 +0800

    add modules: dashboard, library, credentials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AppComponent} from './app.component';
+import {LibraryComponent} from './library/library.component';
+import {CredentialsComponent} from './credentials/credentials.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent}
+];
+
+const childroutes: Routes = [
+  {path: 'dashboard', component: DashboardComponent, outlet: 'navcontent'},
+  {path: 'library', component: LibraryComponent, outlet: 'navcontent'},
+  {path: 'credentials', component: CredentialsComponent, outlet: 'navcontent'},
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), RouterModule.forChild(childroutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   title = 'shield-console';
+  mobileQuery: MediaQueryList;
+
+  // fillerNav = [
+  //   'Dashboard',
+  //   'Library',
+  //   'Credentials'
+  // ];
+
+  fillerContent = Array.from({length: 50}, () =>
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private media: MediaMatcher,
+    private router: ActivatedRoute
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+
 }
